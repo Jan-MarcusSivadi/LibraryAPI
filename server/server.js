6e212d8c8e634eef4a9070df875e8d006d8f7068
Jan-MarcusSivadi <jmarcus@hot.ee>
2023-09-14 12:14:45 +0300

    Project setup #186040124

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env.example') });
+const port = process.env.PORT || 3000
+
+const express = require('express');
+const app = express();
+
+app.use('/pub', express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+
+// GET http://localhost:5000/
+app.get('/', async (req, res) =>{
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`));
