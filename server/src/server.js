e4f7591e3814594fc4d0620888e963b39695ba6c
Hendris <maarhendris@gmail.com>
2023-09-21 12:26:39 +0300

    Removed excess tags to prevent duplicate list

--- server/src/server.js
+++ server/src/server.js
@@ -20,8 +20,14 @@ app.get('/', async (req, res) => {
 
 app.get('/users', (req, res) => {
   res.send([
-    {id:1,firstname:"Jeff",lastname:"Bezos",email:"jeffbezos@amazon.com",password:"markzuckerbergsux",username:"bigbezos420",phonenr:"+1 11748794 "},
-    {id:2,firstname:"Mark",lastname:"Zuckerberg",email:"markzuckerberg@facebook.com",password:"jeffbezossux",username:"markzuckofficial",phonenr:"+1 82543794 "}])
+    {id:1,firstname:"Jeff",lastname:"Bezos",email:"jeffbezos@amazon.com",password:"markzuckerbergsux",username:"bigbezos420",phonenr:"+1 11748794"},
+    {id:2,firstname:"Mark",lastname:"Zuckerberg",email:"markzuckerberg@facebook.com",password:"jeffbezossux",username:"markzuckofficial",phonenr:"+1 82543794"},
+    {id:3,firstname:"Fred",lastname:"Durst",email:"limp@bizkit.com",password:"noookie333",username:"LimpBizkut",phonenr:"+1 56663342"},
+    {id:4,firstname:"Sam",lastname:"Hyde",email:"samhyde@gmail.com",password:"idubbzgfishot32",username:"300yearoldvampire",phonenr:"+1 76532356"}])
+  })
+
+app.get('/users/:id', (req, res) => {
+  res.send(games[req.params.id])
 })
 
 app.listen(port, () => console.log(`listening on port http://localhost:${port}`));
