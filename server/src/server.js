1ffec983bde23767d1f64b03cb434231f54f7db2
Jan-MarcusSivadi <jmarcus.sivad@gmail.com>
2023-09-21 10:35:30 +0300

    change package.json dependencies + update .vs-code/launch.json [#186040124]

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,20 @@
 const path = require('path')
-const swaggerUI = require('swagger-ui-express')
 
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env.example') });
 const port = process.env.PORT || 3000
+const swaggerUI = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json')
 
-app.use ('/docs', swaggerIO.serve. swaggerUi.setup(swaggerDocument))
 
 const express = require('express');
 const app = express();
 
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use('/pub', express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 
 // GET http://localhost:5000/
-app.get('/', async (req, res) =>{
+app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
