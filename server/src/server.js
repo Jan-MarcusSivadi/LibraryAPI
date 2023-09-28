d604f2e5db4c317b986d96a419f68f97434c41e9
Jan-Marcus Sivadi <jmarcus.sivadi@gmail.com>
2023-09-28 10:13:01 +0300

    Revert "Get user details 186070936"

--- server/src/server.js
+++ server/src/server.js
@@ -3,9 +3,8 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env.example') });
 const port = process.env.PORT || 3000
 const swaggerUI = require('swagger-ui-express')
-const yamljs = require('yamljs')
-const swaggerDocument = yamljs.load(__dirname + '/docs/swagger.yaml');
-let users = require("./users/data")
+const swaggerDocument = require('./docs/swagger.json')
+
 
 const express = require('express');
 const app = express();
@@ -19,14 +18,4 @@ app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
-app.get('/users', (req, res) => {
-  res.send(users.getAll())
-  })
-
-app.get('/users/:id', (req, res) => {
-  const getUser = users.getById(req.params.id)
-  if (getUser === undefined) return res.status(404).send({error: "Not found"})
-  res.send(getUser)  
-})
-
-app.listen(port, () => console.log(`listening on port http://localhost:${port}`));
+app.listen(port, () => console.log(`listening on port ${port}`));
