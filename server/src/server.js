83ef7000df678e45ccdeaaca06a252b3769ed58e
Hendris <maarhendris@gmail.com>
2023-10-12 10:11:27 +0300

    Add ORM functionality to project

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,24 @@
 const path = require('path')
 
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env.example') });
-const port = process.env.PORT || 3000
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+const port = process.env.PORT
 const swaggerUI = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load(__dirname + '/docs/swagger.yaml');
 let users = require("./users/data")
 const books = require('./books/data')
+const {Sequelize} = require("sequelize")
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect:"mariadb"
+})
+try {
+  sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.')
+});
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 const express = require('express');
 const app = express();
